212f77871299669c0c1edd25853a1443cf745aee
Louis Kottmann <louis.kottmann@gmail.com>
2013-10-11 20:21:20 +0200

    removed slime from home install

--- init.el
+++ init.el
@@ -4,10 +4,10 @@
 (load-theme 'solarized-light t)
 
 ;; Slime support
-(add-to-list 'load-path "~/.emacs.d/personal/slime/")
-(require 'slime)
-(add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
-(add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
+;; (add-to-list 'load-path "~/.emacs.d/personal/slime/")
+;; (require 'slime)
+;; (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
+;; (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
 ;; Optionally, specify the lisp program you are using. Default is "lisp"
 ;;(setq inferior-lisp-program "yourlisp")
 
