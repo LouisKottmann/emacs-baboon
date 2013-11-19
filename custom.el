a9c49076c7fdfed6063dd373b0273d0bea54770f
Louis Kottmann <louis.kottman@cpexterne.org>
2013-11-19 14:51:33 +0100

    commented out minor modes in modeline, added mail support

--- custom.el
+++ custom.el
@@ -13,7 +13,12 @@
  '(ecb-new-ecb-frame nil)
  '(ecb-options-version "2.40")
  '(ecb-other-window-behavior (quote only-edit))
- '(ecb-source-path (quote (("/" "/") ("/home/louis/JS/webapp" "webapp"))))
+ '(ecb-source-path
+   (quote
+    (("/" "/")
+     ("/home/louis/.emacs.d/personal" "emacs")
+     ("/home/louis/JS/webapp" "webapp")
+     ("/home/louis/JS/swag" "swag"))))
  '(ecb-windows-width 0.15)
  '(emms-stream-repeat-p t)
  '(erc-autojoin-mode t)
@@ -24,12 +29,107 @@
  '(erc-port 6667)
  '(erc-reuse-buffers nil)
  '(global-mark-ring-max 64)
+ '(gnus-select-method
+   (quote
+    (nnimap "gmail"
+            (nnimap-address "imap.gmail.com")
+            (nnimap-server-port 993)
+            (nnimap-stream ssl))))
  '(ibuffer-expert t)
  '(indent-tabs-mode nil)
  '(js-indent-level 2)
+ '(message-send-mail-function (quote smtpmail-send-it))
+ '(mode-line-format
+   (quote
+    ("%e"
+     (:eval
+      (let*
+          ((active
+            (powerline-selected-window-active))
+           (mode-line
+            (if active
+                (quote mode-line)
+              (quote mode-line-inactive)))
+           (face1
+            (if active
+                (quote powerline-active1)
+              (quote powerline-inactive1)))
+           (face2
+            (if active
+                (quote powerline-active2)
+              (quote powerline-inactive2)))
+           (separator-left
+            (intern
+             (format "powerline-%s-%s" powerline-default-separator
+                     (car powerline-default-separator-dir))))
+           (separator-right
+            (intern
+             (format "powerline-%s-%s" powerline-default-separator
+                     (cdr powerline-default-separator-dir))))
+           (lhs
+            (list
+             (powerline-raw "%*" nil
+                            (quote l))
+             (powerline-buffer-size nil
+                                    (quote l))
+             (powerline-raw mode-line-mule-info nil
+                            (quote l))
+             (powerline-buffer-id nil
+                                  (quote l))
+             (when
+                 (and
+                  (boundp
+                   (quote which-func-mode))
+                  which-func-mode)
+               (powerline-raw which-func-format nil
+                              (quote l)))
+             (powerline-raw " ")
+             (funcall separator-left mode-line face1)
+             (when
+                 (boundp
+                  (quote erc-modified-channels-object))
+               (powerline-raw erc-modified-channels-object face1
+                              (quote l)))
+             (powerline-major-mode face1
+                                   (quote l))
+             (powerline-process face1)
+             (powerline-narrow face1
+                               (quote l))
+             (powerline-raw " " face1)
+             (funcall separator-left face1 face2)
+             (powerline-vc face2
+                           (quote r))))
+           (rhs
+            (list
+             (powerline-raw global-mode-string face2
+                            (quote r))
+             (funcall separator-right face2 face1)
+             (powerline-raw "%4l" face1
+                            (quote l))
+             (powerline-raw ":" face1
+                            (quote l))
+             (powerline-raw "%3c" face1
+                            (quote r))
+             (funcall separator-right face1 mode-line)
+             (powerline-raw " ")
+             (powerline-raw "%6p" nil
+                            (quote r))
+             (powerline-hud face2 face1))))
+        (concat
+         (powerline-render lhs)
+         (powerline-fill face2
+                         (powerline-width rhs))
+         (powerline-render rhs)))))))
  '(nxml-child-indent 4)
  '(powerline-default-separator (quote slant))
  '(send-mail-function nil)
+ '(smtpmail-auth-credentials
+   (quote
+    (("smtp.gmail.com" 587 "louis.kottmann@gmail.com" nil))))
+ '(smtpmail-default-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 587)
+ '(smtpmail-starttls-credentials (quote (("smtp.gmail.com" 587 nil nil))))
  '(tabbar-background-color "#EEE8D5")
  '(tabbar-buffer-home-button (quote (("") "")))
  '(tabbar-cycle-scope (quote tabs))
