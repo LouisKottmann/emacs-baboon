932d6b09696439486beb5ad29c97426dd2faf6b3
Louis Kottmann <louis.kottman@cpexterne.org>
2013-12-17 14:06:51 +0100

    added shortcut "C-c . l a" to ecb-activate

--- init.el
+++ init.el
@@ -168,6 +168,7 @@
 (global-set-key (kbd "s-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "s-<down>") 'shrink-window)
 (global-set-key (kbd "s-<up>") 'enlarge-window)
+(global-set-key (kbd "C-c . l a") 'ecb-activate)
 
 
 ;;;init.el ends here
