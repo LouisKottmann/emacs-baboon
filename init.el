3f24ef72088376e7ac0e3d8e15a5aaccc72982df
Louis Kottmann <louis.kottman@cpexterne.org>
2014-03-13 14:25:08 +0100

    do not split words when reaching end of line in ERC

--- init.el
+++ init.el
@@ -153,6 +153,7 @@
    (setq erc-fill-column
          (max 70 (- (/ (frame-width) 2) 10)))))
 (add-hook 'erc-mode-hook 'linum-mode)
+(add-hook 'erc-mode-hook 'visual-line-mode)
 ;; (erc :server "irc.freenode.net" :port "6667" :nick "baboon")
 
 ;; HAML
