f0f297737d7dfc8d9e69e249b38dc4faba4444bf
Louis Kottmann <louis.kottman@cpexterne.org>
2014-06-03 12:32:10 +0200

    prelude-custom.el

--- init.el
+++ init.el
@@ -97,6 +97,7 @@ by Prelude.")
 ;; the core stuff
 (require 'prelude-packages)
 (require 'prelude-ui)
+(require 'prelude-custom)
 (require 'prelude-core)
 (require 'prelude-mode)
 (require 'prelude-editor)
