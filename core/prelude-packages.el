2cee348d2ec6419707ad7ec53f4ec465a82bfffe
Louis Kottmann <louis.kottman@gmail.com>
2014-11-19 18:22:35 +0100

    updated melpa url

--- core/prelude-packages.el
+++ core/prelude-packages.el
@@ -38,7 +38,7 @@
 (setq package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa" . "http://melpa.milkbox.net/packages/")
+     ("melpa" . "http://melpa.org/packages/")
      ("marmalade" . "http://marmalade-repo.org/packages/"))))
 
 (setq package-user-dir (expand-file-name "elpa" prelude-dir))
