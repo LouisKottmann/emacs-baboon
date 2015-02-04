4ee09d50fa9ff8b5e8e2bbee71cb9d691a9391f8
Louis Kottmann <louis.kottmann@gmail.com>
2015-02-04 12:24:11 +0100

    M-; now acts on current-line if no region is selected

--- personal/init.el
+++ personal/init.el
@@ -452,6 +452,25 @@ to the mode-line of windows that are dedicated"
   (interactive)
   (find-file "/ssh:baboon@baboon.io#10522:/home/baboon"))
 
+;; Comment current line/region
+(defun baboon-comment-line-or-region (n)
+  "Comment or uncomment current line and leave point after it.
+With positive prefix, apply to N lines including current one.
+With negative prefix, apply to -N lines above.
+If region is active, apply to active region instead."
+  (interactive "p")
+  (if (use-region-p)
+      (comment-or-uncomment-region
+       (region-beginning) (region-end))
+    (let ((range
+           (list (line-beginning-position)
+                 (goto-char (line-end-position n)))))
+      (comment-or-uncomment-region
+       (apply #'min range)
+       (apply #'max range)))
+    (forward-line 1)
+    (back-to-indentation)))
+
 ;; Smartparens remapping
 (add-hook
  'smartparens-mode-hook
@@ -492,7 +511,7 @@ to the mode-line of windows that are dedicated"
 (global-set-key (kbd "C-<enter>") 'prelude-smart-open-line)
 (global-set-key (kbd "C-S-<enter>") 'prelude-smart-open-line-above)
 (global-set-key (kbd "s-F") 'baboon-refresh-firefox)
-
+(global-set-key (kbd "M-;") 'baboon-comment-line-or-region)
 (key-chord-define-global "KK" 'kill-this-buffer)
 
 ;;;init.el ends here
