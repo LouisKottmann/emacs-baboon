ceaadc6aeb681a8fe4421e1c82d644c234689109
Louis Kottmann <louis.kottmann@gmail.com>
2020-12-08 11:49:06 +0100

    added shell-pop

--- init-20-interface.el
+++ init-20-interface.el
@@ -465,3 +465,16 @@
   :load-path "extras")
 
 (use-package fancy-narrow)
+
+(use-package shell-pop
+  :custom
+  (shell-pop-default-directory "~/GitRepos/hexagonal")
+  (shell-pop-shell-type (quote ("ansi-term" "*ansi-term*" (lambda nil (ansi-term shell-pop-term-shell)))))
+  (shell-pop-term-shell "/bin/bash")
+  (shell-pop-universal-key "C-j")
+  (shell-pop-window-size 30)
+  (shell-pop-full-span t)
+  (shell-pop-window-position "bottom")
+  (shell-pop-autocd-to-working-dir nil)
+  (shell-pop-restore-window-configuration t)
+  (shell-pop-cleanup-buffer-at-process-exit t))
